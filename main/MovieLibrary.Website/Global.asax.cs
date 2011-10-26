e4f151f3e08bd5f7b412335a8199fdef4dac3213
Amir Barylko <amir@barylko.com>
2011-10-26 16:29:47 -0500

    Added route to root, still failing acceptance

--- main/MovieLibrary.Website/Global.asax.cs
+++ main/MovieLibrary.Website/Global.asax.cs
@@ -18,11 +18,11 @@ namespace MovieLibrary.Website
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //routes.MapRoute(
-            //    "Default", // Route name
-            //    "{controller}/{action}/{id}", // URL with parameters
-            //    new { controller = "Movies", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            //);
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Movies", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
 
         }
 
